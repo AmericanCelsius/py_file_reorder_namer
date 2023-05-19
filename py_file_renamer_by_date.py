439702d75fdab3105fc6f844e6b31d58a7e54bee
AmericanCelsius <65144782+AmericanCelsius@users.noreply.github.com>
2023-05-19 17:30:57 -0400

    Update py_file_renamer_by_date.py

--- py_file_renamer_by_date.py
+++ py_file_renamer_by_date.py
@@ -1,13 +1,12 @@
-#MAKE SURE TO BACK UP ALL FILES
+#Back up all files that are inside the directory/ folder you're interacting inside of 
 
 import os
 
-def renamer(file_list,directory):
+def renamer(file_list,directory,index=1):
     print(file_list)
-    index=1
-    for file_name in file_list:
+    for file_name in file_list[index-1:]:
         if file_name[0] == ".":
-            continue  # Skip .DS_Store file
+            continue  # Skips .hidden files 
         file_path = os.path.join(directory, file_name)
         if os.path.isfile(file_path):
             extension = file_name.split(".")[-1].upper()
@@ -17,8 +16,8 @@ def renamer(file_list,directory):
             original_file_path= os.path.join(directory,file_name)
             new_file_path= os.path.join(directory, new_file_name)
             if os.path.exists(new_file_path):
-                print("Skipping {}: File already exists".format(new_file_path))
-                continue
+                print("Trying next number {}: File already exists".format(new_file_path))
+                renamer(file_list,directory,index+1)
             print(new_file_path)
             os.rename(original_file_path, new_file_path)
             index+=1
@@ -33,9 +32,8 @@ def directory(directory):
     renamer(file_list,directory)
     
 
-
-
 def main():
-    directory("") #insert directory
+    location= str(input("Enter location of folder with files: "))
+    directory(location)
 if __name__== "__main__":
     main()
